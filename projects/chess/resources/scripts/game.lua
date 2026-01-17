00e000562f60d240b609cf52d894d71c341a7bc0
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-01-18 00:06:53 +0900

    wip

--- projects/chess/resources/scripts/game.lua
+++ projects/chess/resources/scripts/game.lua
@@ -90,6 +90,9 @@ function Game:render()
     add_piece(BQ, "black_queen_", 1)
     add_piece(BK, "black_king_", 1)
 
+    local offset_x = -3.5
+    local offset_z = -3.5
+
     for rank = 1, 8 do
         for file = 1, 8 do
             local piece = self.board:get_piece(file, rank)
@@ -99,7 +102,8 @@ function Game:render()
                 local piece_entity = pool[#pool]
                 pool[#pool] = nil -- remove from pool
                 local transform = g_scene:get_transform(piece_entity)
-                transform:set_translation(Vector3(file - 1, 0, rank - 1))
+                local position = Vector3(rank - 1 + offset_x, 0, file - 1 + offset_z)
+                transform:set_translation(position)
                 local renderer = g_scene:get_mesh_renderer(piece_entity)
                 renderer:set_visible(true)
             end
