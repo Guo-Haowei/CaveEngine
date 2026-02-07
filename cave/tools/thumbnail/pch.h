f9fee31966795bc6eebee4ec419fb69c084dfbcd
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-02-08 02:34:41 +0900

    delete old thumbnail

--- cave/tools/thumbnail/pch.h
+++
@@ -1,1 +0,0 @@
-#include "engine/private/pch.h"
--- cave/tools/thumbnail/thumbnail_dvars.h
+++
@@ -1,5 +0,0 @@
-#include "engine/private/runtime/dvar/DvarBegin.h"
-
-DVAR_INT(thumbnail_size, DVAR_FLAG_NONE, "Thumbnail size", 256);
-
-#include "engine/private/runtime/dvar/DvarEnd.h"
