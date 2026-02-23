7d97c722130cf76f2e7dc4330494817c26cff1f6
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-02-23 20:54:28 +0900

    more tests

--- projects/chess/src/core/UCI.cpp
+++ projects/chess/src/core/UCI.cpp
@@ -338,6 +338,23 @@ TEST(MoveGen, perft_test_initial_position) {
                     depth,
                     tests);
 }
+
+TEST(MoveGen, perft_test_position2) {
+    std::array<uint64_t, 8> tests{
+        1,
+        48,
+        2039,
+        97862,
+        4085603,
+        193690690,
+        8031647685,
+    };
+
+    constexpr uint8_t depth = 2;
+    PerftTestHelper("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+                    depth,
+                    tests);
+}
 #endif
 
 }  // namespace chess::uci
