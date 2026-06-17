7656c363ec589541a8640d838dfc775298f0de06
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-06-17 16:38:21 +0900

    WIP: scene system

---
+++ cave/engine/private/runtime/scene/SystemManager.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <array>
+
+#include "cave/core/base/NonCopyable.h"
+#include "cave/runtime/scene/ISceneSystem.h"
+
+namespace cave {
+
+class SceneContext;
+
+class SystemManager : public NonCopyable {
+public:
+    SystemManager();
+    ~SystemManager();
+
+    template<SceneSystem T, typename... Args>
+    T& add(Args&&... args) {
+        static_assert(std::is_base_of_v<ISceneSystem, T>);
+        auto system = std::make_unique<T>(std::forward<Args>(args)...);
+        T* raw = system.get();
+        addImpl(std::move(system));
+        return *raw;
+    }
+
+    template<SceneSystem T>
+    T* get() {
+        static_assert(std::is_base_of_v<ISceneSystem, T>);
+        return static_cast<T*>(get(T::kSystemId));
+    }
+
+    template<SceneSystem T>
+    const T* get() const {
+        static_assert(std::is_base_of_v<ISceneSystem, T>);
+        return static_cast<const T*>(get(T::kSystemId));
+    }
+
+    ISceneSystem* get(SceneSystemId id);
+    const ISceneSystem* get(SceneSystemId id) const;
+
+    template<SceneSystem T>
+    bool has() const {
+        return get<T>() != nullptr;
+    }
+
+    bool has(SceneSystemId id) const {
+        return get(id) != nullptr;
+    }
+
+    void onSceneCreate(SceneContext& ctx);
+    void onSceneDestroy();
+
+    void fixedUpdate(float dt);
+    void update(float dt);
+    void lateUpdate(float dt);
+
+    void clear();
+
+private:
+    void addImpl(std::unique_ptr<ISceneSystem>&& system);
+
+private:
+    std::vector<std::unique_ptr<ISceneSystem>> systems_;
+    std::array<ISceneSystem*, std::to_underlying(SceneSystemId::Count)> lookup_{};
+
+    bool scene_created_{ false };
+};
+
+}  // namespace cave
---
+++ cave/engine/public/cave/runtime/scene/ISceneSystem.h
@@ -0,0 +1,73 @@
+// =============================================================================
+// File: cave/runtime/scene/ISceneSystem.h
+// =============================================================================
+#pragma once
+#include <concepts>
+#include <cstdint>
+#include <type_traits>
+#include "cave/core/error/ErrorMacros.h"
+
+namespace cave {
+
+class SceneContext;
+
+enum class SceneSystemId : uint32_t {
+    Invalid = 0,
+
+    TileWorld,
+    NativeScript,
+    LuaScript,
+    Physics2D,
+    Physics3D,
+
+    Count,
+};
+
+#define CAVE_SCENE_SYSTEM(VALUE)                              \
+public:                                                       \
+    static constexpr ::cave::SceneSystemId kSystemId = VALUE; \
+    ::cave::SceneSystemId systemId() const override {         \
+        return kSystemId;                                     \
+    }
+
+class ISceneSystem {
+public:
+    virtual ~ISceneSystem() = default;
+
+    void attach(SceneContext& ctx) {
+        context_ = &ctx;
+        onAttach();
+    }
+
+    void detach() {
+        onDetach();
+        context_ = nullptr;
+    }
+
+    virtual void fixedUpdate(float dt) {}
+    virtual void update(float dt) {}
+    virtual void lateUpdate(float dt) {}
+
+    virtual SceneSystemId systemId() const = 0;
+
+protected:
+    SceneContext& context() {
+        DEV_ASSERT(context_);
+        return *context_;
+    }
+
+    virtual void onAttach() {}
+    virtual void onDetach() {}
+
+private:
+    SceneContext* context_{ nullptr };
+};
+
+template<typename T>
+concept SceneSystem =
+    std::derived_from<T, ISceneSystem> &&
+    requires {
+        { T::kSystemId } -> std::convertible_to<SceneSystemId>;
+    };
+
+}  // namespace cave
