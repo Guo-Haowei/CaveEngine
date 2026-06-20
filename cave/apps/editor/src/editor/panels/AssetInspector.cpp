b92eea0eaedd5d7a512af5599d5a144d931cc249
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-06-20 23:29:49 +0900

    WIP: fix animation

--- cave/apps/editor/src/editor/panels/AssetInspector.cpp
+++ cave/apps/editor/src/editor/panels/AssetInspector.cpp
@@ -15,6 +15,7 @@
 #include "engine/private/ui/inputs.h"
 #include "engine/private/ui/layout.h"
 #include "engine/private/runtime/assets/ImageAsset.h"
+#include "engine/private/runtime/assets/SpriteAnimationAsset.h"
 
 #include "editor/EditorState.h"
 
@@ -213,6 +214,184 @@ void AssetInspector::drawTileSet(IDocument& doc) {
     }
 }
 
+static void ImageSourceDropTarget(IDocument& doc, uint64_t checkerboard) {
+    auto asset = doc.handle<SpriteAnimationAsset>().Get();
+    DEV_ASSERT(asset);
+
+    ImGui::Text("Source Image");
+
+    auto image_handle = asset->GetImageHandle();
+    ImageAsset* image = image_handle.Get();
+
+    Vec2f region_size(128, 128);
+    ui::CenteredImage(image, region_size, checkerboard);
+
+    if (auto _handle = DragDropTarget(AssetType::Image); _handle.is_some()) {
+        asset->SetGuid(_handle.unwrap_unchecked().GetGuid());
+    }
+}
+
+static void DrawFrameSelector(ImageAsset& image_asset, SpriteAnimationContext& ctx) {
+    // @TODO: refactor this, this is the same as ViewerTab::DrawToolBar
+    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+    auto& colors = ImGui::GetStyle().Colors;
+    const auto& button_hovered = colors[ImGuiCol_ButtonHovered];
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(button_hovered.x, button_hovered.y, button_hovered.z, 0.5f));
+    const auto& button_active = colors[ImGuiCol_ButtonActive];
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(button_active.x, button_active.y, button_active.z, 0.5f));
+
+    ui::TextBox("name", ctx.clip_name.data(), ctx.clip_name.capacity(), true);
+
+    ImGui::SameLine();
+
+    if (ImGui::Button(ICON_FA_SQUARE_PLUS "  Add Animation")) {
+        Handle<SpriteAnimationAsset> handle;
+        if (auto anim = handle.Get(); anim) {
+            Handle<ImageAsset> image_handle = anim->GetImageHandle();
+            if (auto image = image_handle.Get()) {
+                const auto [w, h] = ctx.sprite_selector.GetDim();
+                const float inv_w = 1.0f / w;
+                const float inv_h = 1.0f / h;
+                const auto& frame_indices = ctx.sprite_selector.GetSelections();
+                std::vector<Box2> frames;
+                frames.reserve(frame_indices.size());
+                for (const auto [x, y] : frame_indices) {
+#if 0
+                    const float u0 = (x + 0) * inv_w;
+                    const float v0 = (y + 0) * inv_h;
+                    const float u1 = (x + 1) * inv_w;
+                    const float v1 = (y + 1) * inv_h;
+#else
+                    const float u0 = (x + 0) * inv_w;
+                    const float v0 = (y + 1) * inv_h;
+                    const float u1 = (x + 1) * inv_w;
+                    const float v1 = (y + 0) * inv_h;
+#endif
+
+                    frames.push_back({ { u0, v0 }, { u1, v1 } });
+                }
+
+                if (!ctx.clip_name.empty() && !frames.empty()) {
+                    anim->AddClip(std::string(ctx.clip_name.data()), std::move(frames));
+                    ctx.clip_name.clear();
+                    ctx.sprite_selector.ClearSelections();
+
+                    // @TODO: set dirty
+                    // doc.SetDirty();
+                }
+            }
+        }
+    }
+
+    ImGui::PopStyleColor(3);
+    // -------------
+
+    ImGui::BeginGroup();
+
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 4));
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(10, 0));
+
+    ImGui::Dummy(ImVec2(8, 8));
+
+    ctx.sprite_selector.SelectSprite(image_asset, nullptr, nullptr);
+
+    ImGui::PopStyleVar(2);
+
+    ImGui::EndGroup();
+}
+
+static void DrawTimeLine() {
+#if 0
+    constexpr int width = 300;
+
+    ImGui::Columns(2);
+    ImGui::SetColumnWidth(0, width);
+    {
+        SpriteAnimatorComponent* animator = m_tmp_scene->GetComponent<SpriteAnimatorComponent>(m_animator_id);
+        DEV_ASSERT(animator);
+
+        int current_clip = -1;
+        std::vector<const char*> clips;
+        Handle<SpriteAnimationAsset> handle = m_document->GetHandle<SpriteAnimationAsset>();
+        if (auto anim = handle.Get(); anim) {
+            for (const auto& [key, value] : anim->GetClips()) {
+                if (key == animator->GetCurrentClip()) {
+                    current_clip = static_cast<int>(clips.size());
+                }
+                clips.push_back(key.c_str());
+            }
+        }
+
+        const int old_clip = current_clip;
+
+        const char* current_item = current_clip == -1 ? "select clip ..." : clips[current_clip];
+        const int clip_count = static_cast<int>(clips.size());
+        if (ImGui::BeginCombo("Clips", current_item)) {
+            for (int n = 0; n < clip_count; ++n) {
+                const bool is_selected = (current_clip == n);
+                if (ImGui::Selectable(clips[n], is_selected)) {
+                    current_clip = n;
+                }
+
+                if (is_selected) {
+                    ImGui::SetItemDefaultFocus();
+                }
+            }
+            ImGui::EndCombo();
+        }
+
+        if (old_clip != current_clip) {
+            LOG_OK("Set clip to {}", clips[current_clip]);
+            animator->SetClip(clips[current_clip]);
+        }
+    }
+
+    ImGui::NextColumn();
+    SpriteAnimatorComponent* animator = m_tmp_scene->GetComponent<SpriteAnimatorComponent>(m_animator_id);
+    DEV_ASSERT(animator);
+
+    std::vector<const ToolBarButtonDesc*> buttons = {
+        animator->IsPlaying() ? &m_pause_button : &m_play_button
+    };
+
+    DrawToolBar(buttons);
+
+    ImGui::Columns(1);
+
+    // time line
+    float& playback = animator->GetPlaybackTimer();
+    if (ImGui::SliderFloat("timeline", &playback.timer, playback.start, playback.end)) {
+        animator->SetPlaying(true);
+    }
+#endif
+}
+
+void AssetInspector::drawSpriteAnimation(IDocument& doc) {
+    auto sprite_animation = doc.handle<SpriteAnimationAsset>().Get();
+    DEV_ASSERT(sprite_animation);
+
+    auto image_handle = sprite_animation->GetImageHandle();
+
+    if (ImGui::BeginTabBar("##MyTabs1")) {
+        if (ImGui::BeginTabItem("Animation")) {
+            ImageSourceDropTarget(doc, checkerboard_);
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
+    }
+
+    ImGui::Separator();
+    sprite_animation_ctx_.sprite_selector.EditSprite(nullptr, nullptr);
+
+    if (ImageAsset* image = image_handle.Get()) {
+        ImGui::Separator();
+        DrawFrameSelector(*image, sprite_animation_ctx_);
+    }
+
+    ImGui::Separator();
+    DrawTimeLine();
+}
+
 void AssetInspector::drawUIImpl() {
     DocId doc_id = editor_services_.workspace().focusedDoc();
     IDocument* doc = editor_services_.document().resolve(doc_id);
@@ -232,6 +411,9 @@ void AssetInspector::drawUIImpl() {
         } break;
         case AssetType::TileSet: {
             drawTileSet(*doc);
+        } break;
+        case AssetType::SpriteAnimation: {
+            drawSpriteAnimation(*doc);
         } break;
         default:
             break;
