72ebc4dddbf4d4757618f087e9f17d42edbe8207
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-07-13 20:38:51 +0900

    remove chess states

--- projects/chess/src/chess/game/ChessGameMode.cpp
+++ projects/chess/src/chess/game/ChessGameMode.cpp
@@ -1,65 +1,29 @@
 #include "ChessGameMode.h"
 
-#include <format>
-
 #include "cave/core/diagnostics/DebugIdAllocator.h"
-#include "cave/core/diagnostics/ILogSink.h"
-#include "cave/core/error/ErrorMacros.h"
-#include "cave/core/typedefs.h"
-#include "cave/runtime/framework/EngineServices.h"
 
 #include "chess/game/ChessGameSession.h"
-#include "chess/game/ChessIntent.h"
-#include "chess/states/MainMenuState.h"
 
 namespace chess {
 
 using namespace cave;
 
-ChessGameMode::ChessGameMode(SceneRuntime& runtime, IntentBus& intent_bus)
-    : m_runtime(runtime)
-    , m_intent_bus(intent_bus)
-    , m_debug_id(MakeDebugId(this)) {
-    m_intent_bus.addHandler<ChessStateIntent>(this);
+ChessGameMode::ChessGameMode(SceneRuntime& runtime, IntentBus& intent_bus) {
+    m_session = MakeOwner<ChessGameSession>(runtime, intent_bus);
 }
 
-ChessGameMode::~ChessGameMode() {
-    m_intent_bus.removeHandler<ChessStateIntent>(this);
-}
+ChessGameMode::~ChessGameMode() = default;
 
 void ChessGameMode::onEnter() {
-    m_state = std::make_unique<MainMenuState>(m_runtime, m_intent_bus);
-    m_state->onEnter();
+    m_session->onEnterBoot();
 }
 
 void ChessGameMode::onExit() {
+    m_session.reset();
 }
 
-void ChessGameMode::tick(float dt) {
-    if (DEV_VERIFY(m_state)) {
-        m_state->tick(dt);
-        m_intent_bus.flush();
-    }
-}
-
-bool ChessGameMode::handleIntent(Intent& intent) {
-    if (auto state_intent = dynamic_cast<ChessStateIntent*>(&intent)) {
-        commitStateChange(std::move(state_intent->m_state));
-        return true;
-    }
-
-    return false;
-}
-
-void ChessGameMode::commitStateChange(Owner<IChessGameState>&& new_state) {
-    DEV_ASSERT(new_state != nullptr);
-
-    if (m_state) {
-        m_state->onExit();
-    }
-
-    m_state = std::move(new_state);
-    m_state->onEnter();
+void ChessGameMode::tick(float) {
+    m_session->tick();
 }
 
 }  // namespace chess
--- projects/chess/src/chess/game/ChessIntent.cpp
+++ projects/chess/src/chess/game/ChessIntent.cpp
@@ -1,19 +1,6 @@
 #include "ChessIntent.h"
 
-#include "chess/states/IChessGameState.h"
-
 namespace chess {
-
-ChessStateIntent::ChessStateIntent(std::unique_ptr<IChessGameState> state)
-    : m_state(std::move(state))
-    , m_debug_name(m_state->debugName()) {
-}
-
-ChessStateIntent::~ChessStateIntent() = default;
-
-std::string ChessStateIntent::debugString() const {
-    return std::format("->{}", m_debug_name);
-}
 
 std::string MoveIntentBase::debugString() const {
     return std::format("p={} mv={}", std::to_underlying(m_side), m_move.uci());
