f88b34cf81e9b8b83268fddb42333ef6177885e6
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-07-17 15:46:07 +0900

    update scene

--- projects/chess/src/chess/game/MainMenu.cpp
+++ projects/chess/src/chess/game/MainMenu.cpp
@@ -1,5 +1,6 @@
 #include "MainMenu.h"
 
+#include "cave/core/diagnostics/Log.h"
 #include "cave/runtime/scene/ISceneTransitionRequests.h"
 
 #include "ChessSpawner.h"
@@ -7,9 +8,10 @@
 namespace chess {
 
 using namespace ::cave;
-using namespace ::cave::literals;
 
-constexpr StringId kMenuStart = "main_menu.start"_sid;
+constexpr StringId kMainMenuLocal = CAVE_SID("main_menu.local");
+constexpr StringId kMainMenuOnline = CAVE_SID("main_menu.online");
+constexpr StringId kMainMenuQuit = CAVE_SID("main_menu.quit");
 
 void MainMenu::alwaysRun(cave::SceneCommandWriter& writer) {
     Spawner spawner(SpawnType::MainMenu, query(), writer);
@@ -17,15 +19,23 @@ void MainMenu::alwaysRun(cave::SceneCommandWriter& writer) {
 }
 
 void MainMenu::start() {
-    m_start_listener = message().listen(kMenuStart, [this](const Message&) {
+    m_local_listener = message().listen(kMainMenuLocal, [this](const Message&) {
         if (DEV_VERIFY(transition())) {
             transition()->requestSceneChange("@res://Gameplay.scene");
         }
     });
+    m_online_listener = message().listen(kMainMenuOnline, [this](const Message&) {
+        LOG_OK(LogChannel::Game, "main_menu.online received");
+    });
+    m_quit_listener = message().listen(kMainMenuQuit, [this](const Message&) {
+        LOG_OK(LogChannel::Game, "main_menu.quit received");
+    });
 }
 
 void MainMenu::destroy() {
-    message().disconnect(m_start_listener);
+    message().disconnect(m_local_listener);
+    message().disconnect(m_online_listener);
+    message().disconnect(m_quit_listener);
 }
 
 }  // namespace chess
--- projects/chess/src/chess/game/MainMenu.h
+++ projects/chess/src/chess/game/MainMenu.h
@@ -13,7 +13,9 @@ private:
     void destroy() override;
 
 private:
-    uint64_t m_start_listener = 0;
+    uint64_t m_local_listener = 0;
+    uint64_t m_online_listener = 0;
+    uint64_t m_quit_listener = 0;
 };
 
 }  // namespace chess
